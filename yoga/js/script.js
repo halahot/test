3c243d17f8a6831ee38da71fb6586fbfe310debf
Max <smouge1989@mail.ru>
2019-05-20 22:22:15 +0500

    promise

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -118,6 +118,9 @@ form.addEventListener('submit', function(event) {
     event.preventDefault();
     form.appendChild(statusMessage);
 
+    function sendForm() {
+
+    return new Promise(function(resolve, reject) {   
     let request = new XMLHttpRequest();
     request.open('POST', 'server.php');
     request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
@@ -125,15 +128,25 @@ form.addEventListener('submit', function(event) {
     let formData = new FormData(form);
     request.send(formData);
 
-    request.addEventListener('readystatechange', function() {
-        if (request.readyState < 4) {
-        statusMessage.innerHTML = message.loading;
-        } else if (request.readyState === 4 && request.status == 200) {
-            statusMessage.innerHTML = message.success;
+    request.onload = function() {
+         if (request.readyState === 4 && request.status == 200) {
+             resolve();
         } else {
-            statusMessage.innerHTML = message.failure;
+             reject(); 
         }
+    }
+
     });
+    };
+
+    sendForm()
+    .then(() => {
+       statusMessage.innerHTML = message.success; 
+    })
+    .catch(() => {
+        statusMessage.innerHTML = message.failure;
+    })
+    
 
     for(let i = 0; i < input.length; i++) {
         input[i].value = '';
