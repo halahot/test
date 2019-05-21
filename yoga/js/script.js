99266fc3e879526f4f199db074863d76cc9d863e
Max <smouge1989@mail.ru>
2019-05-21 23:02:59 +0500

    calculator

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -205,4 +205,46 @@ form.addEventListener('submit', function(event) {
         }
     });
 
+    //calc
+
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        restDay = document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personSum = 0,
+        daysSum = 0,
+        total = 0;
+        
+    totalValue.innerHTML = 0;
+    
+    persons.addEventListener('change', function() {
+        personSum = +this.value;
+        total = (personSum + daysSum) * 4000;
+
+        if(restDay.value == '' || persons.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            totalValue.innerHTML = total;
+        }
+    });
+
+    restDay.addEventListener('change', function() {
+        daysSum = +this.value;
+        total = (personSum + daysSum) * 4000;
+
+        if(persons.value == '' || restDay.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            totalValue.innerHTML = total;
+        }
+    });
+
+    place.addEventListener('change', function() {
+        if (restDay.value == '' || persons.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            let a  = total;
+            totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+        }
+    });
 });
